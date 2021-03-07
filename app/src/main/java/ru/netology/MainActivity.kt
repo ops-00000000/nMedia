b475412aec44b1ccb67e47d78d266f2049a65bd5
Vladimir <belaevvova999@gmail.com>
2021-03-07 22:18:57 +0300

    Eleven commit

--- app/src/main/java/ru/netology/MainActivity.kt
+++ app/src/main/java/ru/netology/MainActivity.kt
@@ -2,10 +2,25 @@ package ru.netology
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import ru.netology.R.drawable.ic_baseline_favorite_24
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    val post = Post(
+            getString(R.string.content),
+            false
+    )
+  val likeView = findViewById<ImageView>(R.id.imageView)
+    likeView.setOnClickListener {
+        post.liked = !post.liked
+        (it as ImageView).setImageResource(if (post.liked) ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24)
+    }
     }
 }
+
---
+++ app/src/main/java/ru/netology/Post.kt
@@ -0,0 +1,6 @@
+package ru.netology
+
+data class Post(
+   val content: String,
+    var liked: Boolean
+)
